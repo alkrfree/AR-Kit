f9b1f9e7a8fad0317c10e053bd46af6a3c3f55f5
Oleksii Kryzhanovskyi <oleksii.kryzhanovskyi@quartsoft.com>
2025-01-03 13:22:28 +0200

    Added simple XR scene state with gltf loader

---
+++ Assets/ChessBoardDemo/Scripts/DataLoader.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using GLTFast;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour
+{
+  public GltfAsset GltfAsset;
+
+
+  private void Start()
+  {
+    //GltfAsset.Lo
+  }
+}
