6f2adba6d0f02f16f4dfa5c193f4095640a9e99d
Oleksii Kryzhanovskyi <oleksii.kryzhanovskyi@quartsoft.com>
2025-01-14 03:34:52 +0200

    Implemented an extra 'try-catch' for better error handling
    Change the text in the scene if the data wasn't loaded

--- Assets/ChessBoardDemo/Scripts/MarkerController.cs
+++ Assets/ChessBoardDemo/Scripts/MarkerController.cs
@@ -2,47 +2,66 @@
 
 namespace ChessBoardDemo.Scripts
 {
-    public class MarkerController : MonoBehaviour
+  public class MarkerController : MonoBehaviour
+  {
+    private const string WaitingForMarker = "Waiting for Marker";
+    private const string PleaseClickThePlaneToInstantiateTheMarker = "Please click the plane to instantiate the marker";
+    private const string LoadingGeometry = "Loading geometry";
+    private const string PleaseWaitWhileGeometryIsBeingLoaded = "Please wait while geometry is being loaded";
+
+    private const string DataCantBeLoaded = "Data can't be loaded";
+    private const string Sorry = "I'm so sorry";
+
+
+    [SerializeField] private InputManager _inputManager;
+    [SerializeField] private MarkerStateUI _markerStateUI;
+    [SerializeField] private ObjectFactory _objectFactory;
+
+
+    private void Start()
     {
-        private const string WaitingForMarker = "Waiting for Marker";
-        private const string PleaseClickThePlaneToInstantiateTheMarker = "Please click the plane to instantiate the marker";
-        private const string LoadingGeometry = "Loading geometry";
-        private const string PleaseWaitWhileGeometryIsBeingLoaded = "Please wait while geometry is being loaded";
-        
-        
-        [SerializeField] private InputManager _inputManager;
-        [SerializeField] private MarkerStateUI _markerStateUI;
-        [SerializeField] private ObjectFactory _objectFactory;
+      ChangeMarkerTextBeforeCreation();
 
-
-        private void Start()
-        {
-            ChangeMarkerTextBeforeCreation();
-            
-            _inputManager.MarkerCreated += ChangeMarkerTextAfterCreation;
-            _inputManager.MarkerCreated += AddListenersToMarker;
-        }
-
-        private void AddListenersToMarker() => 
-            _objectFactory.GetMarker().DataLoaded += ChangeMarkerTextAfterDataLoaded;
-
-        private void ChangeMarkerTextBeforeCreation()
-        {
-            _markerStateUI.ChangeStateText(WaitingForMarker);
-            _markerStateUI.ChangeClickText(PleaseClickThePlaneToInstantiateTheMarker);
-         
-        }
-        
-        private void ChangeMarkerTextAfterCreation()
-        {
-            _markerStateUI.ChangeStateText(LoadingGeometry);
-            _markerStateUI.ChangeClickText(PleaseWaitWhileGeometryIsBeingLoaded);
-        }
-        
-        private void ChangeMarkerTextAfterDataLoaded()
-        {
-            _markerStateUI.ChangeStateText("");
-            _markerStateUI.ChangeClickText("");
-        }
+      _inputManager.MarkerCreated += AfterMarkerCreated;
     }
+
+
+    private void AfterMarkerCreated()
+    {
+      ChangeMarkerTextAfterCreation();
+      AddListenersToMarker();
+
+      _inputManager.MarkerCreated -= AfterMarkerCreated;
+    }
+
+    private void AddListenersToMarker()
+    {
+      _objectFactory.GetMarker().DataLoaded += ChangeMarkerTextAfterDataLoaded;
+      _objectFactory.GetMarker().DataCantBeLoaded += ChangeTextIfDataCantBeLoaded;
+    }
+
+    private void ChangeMarkerTextBeforeCreation()
+    {
+      _markerStateUI.ChangeStateText(WaitingForMarker);
+      _markerStateUI.ChangeClickText(PleaseClickThePlaneToInstantiateTheMarker);
+    }
+
+    private void ChangeMarkerTextAfterCreation()
+    {
+      _markerStateUI.ChangeStateText(LoadingGeometry);
+      _markerStateUI.ChangeClickText(PleaseWaitWhileGeometryIsBeingLoaded);
+    }
+
+    private void ChangeTextIfDataCantBeLoaded()
+    {
+      _markerStateUI.ChangeStateText(DataCantBeLoaded);
+      _markerStateUI.ChangeClickText(Sorry);
+    }
+
+    private void ChangeMarkerTextAfterDataLoaded()
+    {
+      _markerStateUI.ChangeStateText("");
+      _markerStateUI.ChangeClickText("");
+    }
+  }
 }
--- Assets/ChessBoardDemo/Scripts/MarkerDataLoader.cs
+++ Assets/ChessBoardDemo/Scripts/MarkerDataLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using GLTFast;
 using Newtonsoft.Json.Linq;
@@ -8,108 +9,129 @@ using UnityEngine;
 
 namespace ChessBoardDemo.Scripts
 {
-    public class MarkerDataLoader : MonoBehaviour
+  public class MarkerDataLoader : MonoBehaviour
+  {
+    private const string URL =
+      "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/d7a3cc8e51d7c573771ae77a57f16b0662a905c6/2.0/ABeautifulGame/glTF/ABeautifulGame.gltf";
+
+    public event Action DataLoaded;
+    public event Action DataCantBeLoaded;
+
+    [SerializeField] private Transform SpawnPoint;
+
+    private GltfImport gltf;
+    private string _jsonText;
+
+
+    private async void Start()
     {
-        private const string URL =
-            "https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/d7a3cc8e51d7c573771ae77a57f16b0662a905c6/2.0/ABeautifulGame/glTF/ABeautifulGame.gltf";
-
-        public event Action DataLoaded;
-
-        [SerializeField] private Transform SpawnPoint;
-
-        private GltfImport gltf;
-        private string _jsonText;
-    
-
-
-        private async void Start()
-        {
-            await DownloadJsonAsync();
-            await DownloadGltf();
-           
-            Debug.LogError("IsDone");
-        }
-
-        private async Task DownloadGltf()
-        {
-            GltfImport gltfImport = new GltfImport();
-            bool success = await gltfImport.LoadGltfJson(_jsonText, new Uri(URL));
-            await SpawnGeometry(success, gltfImport);
-        }
-
-        private async Task SpawnGeometry(bool success, GltfImport gltfImport)
-        {
-            if (success)
-            {
-                await gltfImport.InstantiateMainSceneAsync(SpawnPoint);
-                DataLoaded?.Invoke();
-            }
-        }
-
-        private async Task DownloadJsonAsync()
-        {
-            try
-            {
-                using (HttpClient client = new HttpClient())
-                {
-                    string jsonText = await client.GetStringAsync(URL);
-                    Debug.Log("JSON loaded: " + jsonText);
-                    _jsonText = jsonText;
-                    ProcessJson(jsonText);
-                }
-            }
-            catch (Exception ex)
-            {
-                Debug.LogError($"Error loading JSON: {ex.Message}");
-            }
-        }
-
-        private void ProcessJson(string jsonText)
-        {
-            JObject jsonData = JObject.Parse(jsonText);
-            string nodeName = "King_B";
-            JToken foundNode = FindNodeByName(jsonData["nodes"], nodeName);
-
-            if (foundNode != null)
-            {
-                Debug.Log($"Object '{nodeName}' found: {foundNode}");
-                var translationArray = foundNode["translation"];
-                if (translationArray != null)
-                {
-                    Vector3 shift = ShiftSpawnPoint(translationArray);
-                    Debug.Log($"Translation vector: {shift}");
-                }
-                else
-                {
-                    Debug.Log($"Translation not found for '{nodeName}'.");
-                }
-            }
-            else
-            {
-                Debug.Log($"Object '{nodeName}' not found.");
-            }
-        }
-
-        private Vector3 ShiftSpawnPoint(JToken translationArray)
-        {
-            float x = translationArray[0].Value<float>();
-            float y = 0;
-            float z = translationArray[2].Value<float>();
-
-            Vector3 shift = new Vector3(x, y, z);
-            SpawnPoint.position -= shift;
-            return shift;
-        }
-
-        private JToken FindNodeByName(JToken nodesArray, string nodeName)
-        {
-            foreach (var node in nodesArray)
-            {
-                if (node["name"] != null && node["name"].ToString() == nodeName)
-                    return node;
-            }
-
-            return null;
-        }
+      try
+      {
+        await DownloadJsonAsync();
+        await DownloadGltf();
+        LoadingDone();
+      }
+      catch (Exception ex)
+      {
+        DataCantBeLoaded?.Invoke();
+      }
     }
+
+    private void LoadingDone()
+    {
+      Debug.Log("IsDone");
+    }
+
+    private async Task DownloadJsonAsync()
+    {
+      try
+      {
+        using (HttpClient client = new HttpClient())
+        {
+          string jsonText = await client.GetStringAsync(URL);
+          Debug.Log("JSON downloaded successfully.");
+          _jsonText = jsonText;
+          ProcessJson(jsonText);
+        }
+      }
+      catch (Exception ex)
+      {
+        Debug.LogError($"Error in DownloadJsonAsync: {ex.Message}");
+        throw;
+      }
+    }
+
+    private async Task DownloadGltf()
+    {
+      try
+      {
+        GltfImport gltfImport = new GltfImport();
+        bool success = await gltfImport.LoadGltfJson(_jsonText, new Uri(URL));
+        Debug.Log("GLTF downloaded successfully.");
+        await SpawnGeometry(success, gltfImport);
+      }
+      catch (Exception ex)
+      {
+        Debug.LogError($"Error in DownloadJsonAsync: {ex.Message}");
+        throw;
+      }
+    }
+
+    private async Task SpawnGeometry(bool success, GltfImport gltfImport)
+    {
+      if (success)
+      {
+        await gltfImport.InstantiateMainSceneAsync(SpawnPoint);
+        DataLoaded?.Invoke();
+      }
+    }
+
+    private void ProcessJson(string jsonText)
+    {
+      JObject jsonData = JObject.Parse(jsonText);
+      string nodeName = "King_B";
+      JToken foundNode = FindNodeByName(jsonData["nodes"], nodeName);
+
+      if (foundNode != null)
+      {
+        Debug.Log($"Object '{nodeName}' found: {foundNode}");
+        var translationArray = foundNode["translation"];
+        if (translationArray != null)
+        {
+          Vector3 shift = ShiftSpawnPoint(translationArray);
+          Debug.Log($"Translation vector: {shift}");
+        }
+        else
+        {
+          Debug.Log($"Translation not found for '{nodeName}'.");
+        }
+      }
+      else
+      {
+        Debug.Log($"Object '{nodeName}' not found.");
+      }
+    }
+
+    private Vector3 ShiftSpawnPoint(JToken translationArray)
+    {
+      float x = translationArray[0].Value<float>();
+      float y = 0;
+      float z = translationArray[2].Value<float>();
+
+      Vector3 shift = new Vector3(x, y, z);
+      SpawnPoint.position -= shift;
+      return shift;
+    }
+
+    private JToken FindNodeByName(JToken nodesArray, string nodeName)
+    {
+      foreach (var node in nodesArray)
+      {
+        if (node["name"] != null && node["name"].ToString() == nodeName)
+          return node;
+      }
+
+      return null;
+    }
+  }
 }
